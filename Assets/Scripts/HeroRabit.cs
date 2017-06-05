140e888a613c1540dfe6e6903eec6c86420c83cf
Settonix <alexbee0910@gmail.com>
2017-06-05 15:40:17 +0300

    update
    problems with jumping((

--- Assets/Scripts/HeroRabit.cs
+++ Assets/Scripts/HeroRabit.cs
@@ -8,6 +8,12 @@ public class HeroRabit : MonoBehaviour {
 	public float maxSpeed = 10;
 	Rigidbody2D myBody = null;
 
+	bool isGrounded = false;
+	bool JumpActive = false;
+	float JumpTime = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
+
 	Animator anim;
 
 	//bool grounded = false;
@@ -34,6 +40,47 @@ public class HeroRabit : MonoBehaviour {
 			myBody.velocity = vel;
 		}
 
+
+		//class HeroRabit, void FixedUpdate()
+		Vector3 from = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 0.1f;
+		int layer_id = 1 << LayerMask.NameToLayer ("Ground");
+		//Перевіряємо чи проходить лінія через Collider з шаром Ground
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		if(hit) {
+			isGrounded = true;
+		} else {
+			isGrounded = false;
+		}
+		//Намалювати лінію (для розробника)
+		Debug.DrawLine (from, to, Color.red);
+
+
+		//Якщо кнопка тільки що натислась
+		if(Input.GetButtonDown("Jump") && isGrounded) {
+			this.JumpActive = true;
+		}
+		if(this.JumpActive) {
+			//Якщо кнопку ще тримають
+			if(Input.GetButton("Jump")) {
+				this.JumpTime += Time.deltaTime;
+				if (this.JumpTime < this.MaxJumpTime) {
+					Vector2 vel = myBody.velocity;
+					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+					myBody.velocity = vel;
+				}
+			} else {
+				this.JumpActive = false;
+				this.JumpTime = 0;
+			}
+		}
+
+		Animator animator = GetComponent<Animator> ();
+		if(this.isGrounded) {
+			animator.SetBool ("jump", false);
+		} else {
+			animator.SetBool ("jump", true);
+		}
 
 
 		SpriteRenderer sr = GetComponent<SpriteRenderer>();
